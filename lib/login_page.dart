8e7f6c297545859f3b36f16b14efd009fd962c3a
kukudasKjs <kjs860721@gmail.com>
2024-09-20 15:55:15 +0900

    Version_5

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,6 +10,18 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
+
+  // FocusNode 추가
+  final FocusNode emailFocusNode = FocusNode();
+  final FocusNode passwordFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    // FocusNode 해제
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +37,16 @@ class _LoginScreenState extends State<LoginScreen> {
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
               child: Image.asset(
-                'assets/green-sea.jpg',
+                'assets/bg_1.jpg',
                 fit: BoxFit.cover,
               ),
             ),
             // Gradient Overlay for a modern look
+
             Container(
               decoration: BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Colors.black.withOpacity(0.3), Colors.blue.withOpacity(0.3)],
+                  colors: [Colors.black.withOpacity(0.3), Colors.black.withOpacity(0.2)],
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
                 ),
@@ -40,14 +54,33 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             // Positioned Text with Modern Style
             Positioned(
-              top: 100,
+              top: 40,
               left: 20,
               child: Text(
-                'Dive',
+                'Giant',
                 style: TextStyle(
-                  fontSize: 120,
+                  fontSize: 140,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white.withOpacity(0.8),
+                  color: Colors.white.withOpacity(0.7),
+                  shadows: const [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.black54,
+                      offset: Offset(5, 5),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 165,
+              left: 100,
+              child: Text(
+                'Scuba',
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white.withOpacity(0.6),
                   shadows: const [
                     Shadow(
                       blurRadius: 10.0,
@@ -101,10 +134,30 @@ class _LoginScreenState extends State<LoginScreen> {
                             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                           ),
                         ),
-                        const Text(
-                          'Not a member? Sign up now',
-                          style: TextStyle(fontSize: 20),
+                        const SizedBox(
+                          height: 20,
                         ),
+                        GestureDetector(
+                          onTap: () {
+                            showSignUp(context);
+                          },
+                          child: const Center(
+                            child: Text.rich(
+                              TextSpan(
+                                text: 'Not a member? ',
+                                style: TextStyle(fontSize: 23, color: Colors.white), // Style for the first part
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: 'Sign up now',
+                                    style: TextStyle(
+                                        fontSize: 23,
+                                        color: Colors.redAccent), // Style for 'Sign up now' with a different color
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
                       ],
                     ),
                   ),
@@ -124,7 +177,7 @@ class _LoginScreenState extends State<LoginScreen> {
       decoration: InputDecoration(
         prefixIcon: Icon(icon, color: Colors.white70),
         labelText: label,
-        labelStyle: TextStyle(color: Colors.white70),
+        labelStyle: const TextStyle(color: Colors.white70),
         filled: true,
         fillColor: Colors.white.withOpacity(0.1),
         enabledBorder: OutlineInputBorder(
@@ -136,7 +189,7 @@ class _LoginScreenState extends State<LoginScreen> {
           borderRadius: BorderRadius.circular(15),
         ),
       ),
-      style: TextStyle(color: Colors.white),
+      style: const TextStyle(color: Colors.white),
       validator: (value) {
         if (value == null || value.isEmpty) {
           return 'Please enter some text';
@@ -144,5 +197,123 @@ class _LoginScreenState extends State<LoginScreen> {
         return null;
       },
     );
+  }
+
+  Widget _signUpTextField({required String label, IconData? icon, bool obscureText = false}) {
+    return TextFormField(
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon, color: Colors.black),
+        labelText: label,
+        labelStyle: const TextStyle(color: Colors.black),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.1),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black.withOpacity(0.1)),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black.withOpacity(0.2)),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+      style: const TextStyle(color: Colors.black),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
+    );
+  }
+
+  showSignUp(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0), // 모서리를 둥글게
+          ),
+          backgroundColor: Colors.white, // 배경색을 부드러운 화이트로 설정
+          child: Padding(
+            padding: const EdgeInsets.all(20.0), // 패딩을 추가하여 공간을 확보
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // 타이틀
+                const Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // 텍스트 필드
+                _signUpTextField(
+                  label: 'Nick Name',
+                  icon: Icons.account_box_rounded,
+                ),
+                const SizedBox(height: 15),
+                _signUpTextField(
+                  label: 'Email',
+                  icon: Icons.email_outlined,
+                ),
+                const SizedBox(height: 15),
+                _signUpTextField(
+                  label: 'Password',
+                  icon: Icons.lock_outline,
+                ),
+                const SizedBox(height: 25),
+                // 버튼들
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        FocusScope.of(context).unfocus();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.greenAccent,
+                        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        'Yes',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        FocusScope.of(context).unfocus();
+                      },
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                        side: const BorderSide(color: Colors.redAccent),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        'No',
+                        style: TextStyle(fontSize: 16, color: Colors.redAccent),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    ).then((_) {
+      FocusScope.of(context).unfocus();
+    });
   }
 }
